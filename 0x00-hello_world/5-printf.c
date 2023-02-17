7a52c1175c739494c20b37735854dbcb4c2f0af2
JooGraced <awuah1132@gmail.com>
2023-02-16 17:30:19 -0800

    We are using the function printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,11 @@
-#include <stdio>
+#include <stdio.h>
 /**
- * main - A program that will make use of printf function
+ * main - A program that makes use of printf function
  * Return: 0 (complete)
  */
 
-int main(void)															
+int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n")							
+	printf("with proper grammar, but the outcome is a piece of art,\n")
 	return (0);
 }
-		
